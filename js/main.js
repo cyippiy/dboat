1693a8b72c74c92826f866285185f96437cd7c1d
Chris Yip <chris.yip87@gmail.com>
2019-07-12 13:36:25 -0700

    youth and adults working on same index page

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 
 let timeChart,formattedData,
     adultData,rawData,
-    adultChart;
+    adultChart, youthData,
+    youthChart;
 
 let time = 0;
 
 d3.json("data/data.json").then(function (data) {
-    // console.log(data);
     rawData = data;
     // Clean data
     formattedData = data.map(function (year) {
@@ -19,18 +19,10 @@ d3.json("data/data.json").then(function (data) {
             return country;
         })
     });
-    // console.log(formattedData);
-    // Run the code every 0.1 second
-    // d3.interval(function () {
-    //     update();
-    // }, 100);
-
-    // timeChart = new TimeChart("#chart-area",formattedData);
 });
 let totalData;
 d3.json("data/adults.json").then((data) => {
     totalData = data;
-    let time;
     adultData = data.map((race) => { 
         race.teams.map(
             team => {
@@ -40,25 +32,31 @@ d3.json("data/adults.json").then((data) => {
             });
         return race;
     })
-    // adultData = data.map((race) => { 
-    //     race.teams.map(
-    //         team => {
-    //                 time = team.time.split(":");
-    //                 team.time = +time[0] * 60 + parseFloat(time[1]);
-    //                 return team;
-    //         });
-    //     return race;
-    // })
+    adultChart = new TimeChart("#chart-area-adult", adultData);
+});
+
+d3.json("data/youth.json").then( (data) => {
+    totalData = data;
+    youthData = data.map((race) => {
+        race.teams.map(
+            team => {
+                time = team.time.split(":");
+                team.time = +time[0] * 60 + parseFloat(time[1]);
+                return team;
+            });
+        return race;
+    });
+
+    youthChart = new TimeChart("#chart-area-youth", youthData);
     d3.interval(function () {
         update();
     }, 1500);
 
-    adultChart = new TimeChart("#chart-area", adultData);
-    // update();
 });
 
 
 function update(){
     // timeChart.wrangleData()
     adultChart.update();
+    youthChart.update();
 }
--- js/timeChart.js
+++ js/timeChart.js
@@ -24,15 +24,14 @@ TimeChart.prototype.initVis = function() {
 
     // Scales
     vis.x = d3.scaleLinear()
-    // vis.x = d3.scaleLog()
         .range([0,vis.width])
-        .domain([0, 10]); // remove later
+        .domain([0, 10]); 
     vis.y = d3.scaleLinear()
         .range([vis.height,0])
-        .domain([110, 180]); // remove later
+        .domain([110, 180]);
     vis.area = d3.scaleLinear()
         .range([0 * Math.PI, 50 * Math.PI])
-        .domain([0, 4]); // remove later
+        .domain([0, 4]);
     vis.colors = {
         "Dragon Warriors":"#FF0011",
         "Bay Area Dragons": "#3187F7",
@@ -40,7 +39,15 @@ TimeChart.prototype.initVis = function() {
         "Lightwave": "#71BEEB",
         "Dragon Max": "#FA8484",
         "Ripple Effect": "#FF9100",
-        "Alameda Dragon Flyers": "#BC2020"
+        "Alameda Dragon Flyers": "#BC2020",
+        "Northwind": "#003366",
+        "East Harbor": "#FF007F",
+        "Lowell": "#FF0000",
+        "Lincoln": "#ECC501",
+        "CYC": "#1C278D",
+        "Washington": "#CA1F1F",
+        "GCD": "#4C0998"
+
     };
 
     // Tooltip
@@ -56,7 +63,6 @@ TimeChart.prototype.initVis = function() {
             strTime += seconds;
             var text = "<strong>Team:</strong> <span style='color:red'>" + d.team + "</span><br>";
             text += "<strong>Time:</strong> <span style='color:red'>" + strTime + "</span><br>";
-            // text += "<strong>Time:</strong> <span style='color:red'>" + d3.format(".2f")(d.time) + "</span><br>";
             text += "<strong>Placement:</strong> <span style='color:red'>" + (d.place) + "</span><br>";
             text += "<strong>Crew Size</strong> <span style='color:red'>" + (d.size) + "</span><br>";
             return text;
@@ -80,7 +86,7 @@ TimeChart.prototype.initVis = function() {
         .text("Time (seconds)")
     vis.timeLabel = vis.g.append("text")
         .attr("y", vis.height - 10)
-        .attr("x", vis.width - 50)
+        .attr("x", vis.width - 60)
         .attr("font-size", "20px")
         .attr("opacity", "0.4")
         .attr("text-anchor", "middle")
@@ -113,22 +119,9 @@ TimeChart.prototype.update = function () {
     let vis = this;
     vis.t = d3.transition()
         .duration(1000);
-    // console.log("hi");
+
     // JOIN new data with old elements.
-    // console.log(vis.data[vis.time]);
-    // let circles = vis.g.selectAll("circle").data(vis.data[vis.time], function (d) {
-    //     console.log(d);
-    //     return d.teams;
-    // });
-    // console.log(circles);
-    // console.log(`this is time: ${vis.time}`);
-    // console.log(vis.data);
     let circles = vis.g.selectAll("circle").data(vis.data[vis.time].teams);
-    // let circles = vis.g.selectAll("circle").data(vis.data, function (d) {
-    //    console.log(d)
-    //    return d.teams
-    // });
-    // console.log(circles);
     // EXIT old elements not present in new data.
     circles.exit()
         .attr("class", "exit")
@@ -141,7 +134,6 @@ TimeChart.prototype.update = function () {
         .attr("fill", function (d) { return vis.colors[d.team]; })
         .on("mouseover", vis.tip.show)
         .on("mouseout", vis.tip.hide)
-        // .attr("fill", function (d) { return vis.teamColor(d.team); })
         .merge(circles)
         .transition(vis.t)
         .attr("cy", function (d) { return vis.y(d.time); })
@@ -152,112 +144,3 @@ TimeChart.prototype.update = function () {
     vis.timeLabel.text(`${vis.data[vis.time].race}`);
     vis.time = (vis.time+1) % vis.data.length;
 };
-
-// TimeChart.prototype.initVis = function() {
-//     let vis = this;
-
-//     vis.time = 0;
-
-//     vis.margin = { left: 80, right: 20, top: 50, bottom: 100 };
-//     vis.height = 500 - vis.margin.top - vis.margin.bottom;
-//     vis.width = 800 - vis.margin.left - vis.margin.right;
-
-//     vis.svg = d3.select(vis.parentElement)
-//         .append("svg")
-//         .attr("width", vis.width + vis.margin.left + vis.margin.right)
-//         .attr("height", vis.height + vis.margin.top + vis.margin.bottom);
-//     vis.g = vis.svg.append("g")
-//         .attr("transform", `translate(${vis.margin.left},${vis.margin.top})`);
-
-//     vis.t = function() { return d3.transition().duration(100); }
-
-//     // Scales
-//     // vis.x = d3.scaleLinear()
-//     vis.x = d3.scaleLog()
-//         .base(10)
-//         .range([0,vis.width])
-//         .domain([142, 150000]); // remove later
-//     vis.y = d3.scaleLinear()
-//         .range([vis.height,0])
-//         .domain([0, 90]); // remove later
-//     vis.area = d3.scaleLinear()
-//         .range([25 * Math.PI, 1500 * Math.PI])
-//         .domain([2000, 1400000000]); // remove later
-//     vis.continentColor = d3.scaleOrdinal(d3.schemePastel1); // remove later
-
-//     // Labels
-//     vis.xLabel = vis.g.append("text")
-//         .attr("y", vis.height + 50)
-//         .attr("x", vis.width / 2)
-//         .attr("font-size", "20px")
-//         .attr("text-anchor", "middle")
-//         .text("GDP Per Capita ($)");
-//     vis.yLabel = vis.g.append("text")
-//         .attr("transform", "rotate(-90)")
-//         .attr("y", -40)
-//         .attr("x", -170)
-//         .attr("font-size", "20px")
-//         .attr("text-anchor", "middle")
-//         .text("Life Expectancy (Years)")
-//     vis.timeLabel = vis.g.append("text")
-//         .attr("y", vis.height - 10)
-//         .attr("x", vis.width - 40)
-//         .attr("font-size", "40px")
-//         .attr("opacity", "0.4")
-//         .attr("text-anchor", "middle")
-//         .text("1800");
-
-
-//     // X Axis
-//     vis.xAxisCall = d3.axisBottom(vis.x)
-//         .tickValues([400, 4000, 40000])
-//         .tickFormat(d3.format("$"));
-//     vis.g.append("g")
-//         .attr("class", "x axis")
-//         .attr("transform", `translate(0,${vis.height})`)
-//         .call(vis.xAxisCall);
-
-//     // Y Axis
-//     vis.yAxisCall = d3.axisLeft(vis.y)
-//         .tickFormat(function (d) { return +d; });
-//     vis.g.append("g")
-//         .attr("class", "y axis")
-//         .call(vis.yAxisCall);
-
-//     vis.wrangleData();
-// };
-
-// TimeChart.prototype.wrangleData = function(){
-//     let vis = this;
-// };
-
-// TimeChart.prototype.update = function () {
-//     let vis = this;
-//     vis.t = d3.transition()
-//         .duration(100);
-
-//     // JOIN new data with old elements.
-//     let circles = vis.g.selectAll("circle").data(vis.data[vis.time], function (d) {
-//         return d.country;
-//     });
-
-//     // EXIT old elements not present in new data.
-//     circles.exit()
-//         .attr("class", "exit")
-//         .remove();
-
-//     // ENTER new elements present in new data.
-//     circles.enter()
-//         .append("circle")
-//         .attr("class", "enter")
-//         .attr("fill", function (d) { return vis.continentColor(d.continent); })
-//         .merge(circles)
-//         .transition(vis.t)
-//         .attr("cy", function (d) { return vis.y(d.life_exp); })
-//         .attr("cx", function (d) { return vis.x(d.income) })
-//         .attr("r", function (d) { return Math.sqrt(vis.area(d.population) / Math.PI) });
-
-//     // Update the time label
-//     vis.timeLabel.text(+(vis.time + 1800));
-//     vis.time = (vis.time < 214) ? vis.time + 1 : 0;
-// };
